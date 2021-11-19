87f900de593a08a7370b03c44087c5e8122a8e1c
kiko <cahenriquesoares@gmail.com>
2021-11-19 00:52:55 -0300

    Resolução do exercício 10_sort_last.

--- 10_sort_last.py
+++ 10_sort_last.py
@@ -10,8 +10,9 @@ Irá retornar: [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
 Dica: Use uma custom key= function para extrair o ultimo elemento de cada tupla.
 """
 def sort_last(tuples):
-    # +++ SUA SOLUÇÃO +++
-    return
+    from operator import itemgetter
+
+    return sorted(tuples, key=itemgetter(-1))
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
